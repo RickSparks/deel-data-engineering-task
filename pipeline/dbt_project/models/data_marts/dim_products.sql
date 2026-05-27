b9e3aff0bcc7ef3d4846078af503ba438bbc9db5
RickSparks <39152600+RickSparks@users.noreply.github.com>
2026-05-28 00:51:31 +0100

    feat: fixing dims

--- pipeline/dbt_project/models/data_marts/dim_products.sql
+++ pipeline/dbt_project/models/data_marts/dim_products.sql
@@ -3,13 +3,13 @@
 {{ config(materialized='table') }}
 
 select
-    {{ dbt_utils.generate_surrogate_key(['product_id']) }}    as product_key,
+    {{ dbt_utils.generate_surrogate_key(['product_id']) }} as product_key,
     product_id,
     product_name,
     barcode,
     unity_price,
     is_active,
-    dbt_valid_from                                            as valid_from,
-    dbt_valid_to                                              as valid_to,
-    case when dbt_valid_to is null then true else false end    as is_current
+    dbt_valid_from as valid_from,
+    dbt_valid_to as valid_to,
+    case when dbt_valid_to is null then true else false end as is_current
 from {{ ref('snap_products') }}
